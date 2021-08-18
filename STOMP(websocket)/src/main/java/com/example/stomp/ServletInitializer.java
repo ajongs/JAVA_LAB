6f8f08b6a91a1de90f9042c4f8518c5c2fc4d138
ajongs <parkwonjong1030@gmail.com>
2021-08-18 19:04:30 +0900

    초기 커밋

---
+++ STOMP(websocket)/src/main/java/com/example/stomp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.stomp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StompApplication.class);
+    }
+
+}
